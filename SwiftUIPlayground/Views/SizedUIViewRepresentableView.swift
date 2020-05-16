b22660284726aeb0fc69f0350f3c498900242ea7
Ralf Ebert <info@ralfebert.de>
2020-05-16 09:14:11 +0200

    solution for SizedUIViewRepresentableView

--- SwiftUIPlayground/Views/SizedUIViewRepresentableView.swift
+++ SwiftUIPlayground/Views/SizedUIViewRepresentableView.swift
@@ -2,6 +2,21 @@
 
 import SwiftUI
 
+// How do you pass the size from UIKit up to SwiftUI in a UIViewRepresentable?
+// https://stackoverflow.com/questions/61832221/passing-uikit-uiview-size-to-swiftui-in-uiviewrepresentable
+struct YellowBoxView: UIViewRepresentable {
+
+    func makeUIView(context: Context) -> YellowBoxUIKitView {
+        let view = YellowBoxUIKitView()
+        view.setContentHuggingPriority(.required, for: .horizontal)
+        view.setContentHuggingPriority(.required, for: .vertical)
+        return view
+    }
+
+    func updateUIView(_ uiView: YellowBoxUIKitView, context: Context) {}
+
+}
+
 /// Some UIKit view that wants to have a specific size
 class YellowBoxUIKitView: UIView {
 
@@ -17,18 +32,6 @@ class YellowBoxUIKitView: UIView {
     override var intrinsicContentSize: CGSize {
         CGSize(width: 100, height: 100)
     }
-
-}
-
-struct YellowBoxView: UIViewRepresentable {
-
-    func makeUIView(context: Context) -> YellowBoxUIKitView {
-        // TODO: How do you pass the size from UIKit up to SwiftUI?
-        // https://stackoverflow.com/questions/61832221/passing-uikit-uiview-size-to-swiftui-in-uiviewrepresentable
-        YellowBoxUIKitView()
-    }
-
-    func updateUIView(_ uiView: YellowBoxUIKitView, context: Context) {}
 
 }
 
