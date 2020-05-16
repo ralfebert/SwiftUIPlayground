88cbc5d73655e831916d62a121d9412e6a4a875b
Ralf Ebert <info@ralfebert.de>
2020-05-16 06:18:22 +0200

    Initial Commit

---
+++ SwiftUIPlayground/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIPlayground
+//
+//  Created by Ralf Ebert on 16.05.20.
+//  Copyright © 2020 Ralf Ebert. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
