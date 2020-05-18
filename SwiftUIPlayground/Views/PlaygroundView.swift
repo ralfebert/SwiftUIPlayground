462e099fd4ddf51970ffa44c3c8aa15de5d3d86d
Ralf Ebert <info@ralfebert.de>
2020-05-18 19:57:14 +0200

    playground view

---
+++ SwiftUIPlayground/Views/PlaygroundView.swift
@@ -0,0 +1,16 @@
+// SwiftUIPlayground
+// https://github.com/ralfebert/SwiftUIPlayground/
+
+import SwiftUI
+
+struct PlaygroundView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PlaygroundView_Previews: PreviewProvider {
+    static var previews: some View {
+        PlaygroundView()
+    }
+}
